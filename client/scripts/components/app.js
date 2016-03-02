d734704b2b0c6791f4be16bed36a39f3f9fea8de
Bartosz Galek <bartosz.galek@allegrogroup.com>
2016-03-02 11:20:11 +0100

    Initial commit

---
+++ client/scripts/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import repositories from '../api/repositories';
+
+export default React.createClass({
+    displayName: 'App',
+
+    render() {
+        return (
+            <div className="app">
+                <h1>App</h1>
+            </div>
+        );
+    }
+});
---
+++ client/scripts/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from '../styles/styles.less';
+
+import App from './components/app';
+
+ReactDOM.render(<App />, document.querySelector('#main'));
